67e92ead9ed3d3dc73420a9614d78cafaa3d83de
Alec DiGirolamo <adigirolamo@carthage.edu>
2020-04-30 10:46:46 -0500

    All tests passed, again

--- LineClass/LineClass/point.hpp
+++ LineClass/LineClass/point.hpp
@@ -5,18 +5,20 @@
 //  Created by Noah Lindner on 4/28/20.
 //  Copyright © 2020 Noah Lindner. All rights reserved.
 //
-
+#pragma once;
 #include <string>
 
 using namespace std;
 
-class point{
+class Point
+{
 private:
     float x;
     float y;
-    
+
 public:
+    float getX();
+    float getY();
     string toString();
-    point(float x, float y);
-    
+    Point(float x, float y);
 };
